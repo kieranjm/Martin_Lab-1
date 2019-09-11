30f8ff3ac3968fa8489529d15f6713d30d048707
kjm61 <kjm61@LAPTOP-90T041MV>
2019-09-10 21:26:55 -0500

    CookieCalories.java is done, but can't figure out how to make it run. Everytime I press "run" a the sales tax program runs.

--- src/main/java/CookieCalories.java
+++ src/main/java/CookieCalories.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,6 +14,15 @@
 public class CookieCalories {
     public static void main(String[] args)
     {
+        Scanner keyboard = new Scanner(System.in);
+        int oreo = 75;
+        int oreosEaten = 0;
+        int totalCalories = 0;
         
+        System.out.println("How many oreos did you eat?");
+        oreosEaten = keyboard.nextInt();
+        totalCalories = oreosEaten * oreo;
+        
+        System.out.println("You consumed " + totalCalories + "calories");
     }
 }

ad4fb83667d2012ff11921e3f5f9985c665ed309
kjm61 <kjm61@LAPTOP-90T041MV>
2019-09-10 21:06:51 -0500

    SalesTax.java complete.

---
+++ src/main/java/SalesTax.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+import static javax.swing.text.html.HTML.Tag.S;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kjm61
+ */
+public class SalesTax {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner (System.in);
+        float county = (float) 0.02;
+        float state = (float) 0.04;
+        float totalTax = 0;
+        float price = 0;
+        
+        System.out.println("Whats the price?");
+        price = keyboard.nextInt();
+        totalTax = state + county;
+        price = (price*totalTax) + price;
+        
+        System.out.println("Your total is " + price);
+    }
+}
